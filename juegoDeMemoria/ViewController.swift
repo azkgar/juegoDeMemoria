80f9348fb3824e7952dded857aeaf47971eaf34d
Azkary Arisbey Garc√≠a Gonz√°lez <azkary_garcia@hotmail.com>
2015-12-01 17:14:04 -0600

    Programa final

--- juegoDeMemoria/ViewController.swift
+++ juegoDeMemoria/ViewController.swift
@@ -12,12 +12,43 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     //modificacion
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+        var num : Int = 0
+        
+        for var i = 0; i <= 100; i++ {
+            
+            let cinco : Int = num % 5
+            
+            let par : Int = num % 2
+            
+            if num >= 30 && num <= 40{
+                
+                print ("\(num) Viva Swift!!! ")
+                
+            }else if cinco == 0{
+                
+                print("\(num) Bingo!!!")
+                
+            }else if par == 0{
+                
+                print("\(num) Par")
+                
+            }else if par != 0{
+                
+                print("\(num) Impar")
+                
+            }
+            
+            ++num
+        }
         // Dispose of any resources that can be recreated.
     }
 
