a902dbca1559f9d28b0dac65ce68485f3cb507d2
Azkary Arisbey Garc√≠a Gonz√°lez <azkary_garcia@hotmail.com>
2015-12-01 17:04:22 -0600

    valida 1

--- juegoDeMemoria/ViewController.swift
+++ juegoDeMemoria/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    //modificacion
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
